e1732ff00b7f6abad5c2c7beddc0c704896e4cf1
FagnerRumenigg <rumeniggmoraes@gmail.com>
2024-08-24 21:38:02 -0300

    passando caminho absoluto do jar

--- src/main/java/entertain_me/app/controller/HomeController.java
+++ src/main/java/entertain_me/app/controller/HomeController.java
@@ -2,11 +2,13 @@ package entertain_me.app.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @Tag(name = "Home")
+@CrossOrigin
 @RestController
 public class HomeController {
 
